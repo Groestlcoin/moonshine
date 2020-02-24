3d5d91b9c03295d0cb64d24068e88246e977b712
HashEngineering <hashengineeringsolutions@gmail.com>
2020-02-23 18:10:05 -0800

    Update icon and splash screen colors

--- ios/moonshine/AppDelegate.m
+++ ios/moonshine/AppDelegate.m
@@ -22,7 +22,7 @@
                                                    moduleName:@"moonshine"
                                             initialProperties:nil];
 
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:0.0f green:0.396078431372549f blue:0.5764705882352941f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
